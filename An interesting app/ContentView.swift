7c7607188b2a5f3475d791e211dfa79f08dec6d9
ariellenicolealbers-sudo <arielle_nicole_albers@s2026.ssts.edu.sg>
2026-09-08 21:09:14 +0800

    Battles

--- An interesting app/ContentView.swift
+++ An interesting app/ContentView.swift
@@ -13,29 +13,105 @@ struct ContentView: View {
             TabView {
                 Tab("Home", systemImage: "house.fill") {
                     VStack {
-                        
-                        Image("Eevee")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 300, height: 300)
-                            .clipShape(Circle())
                         List {
-                            Button("Tackle") {
-                                
-                            }
-                            Button("Swift") {
-                                
-                            }
-                            Button("Double-edge") {
-                                
-                            }
+                            Text("Choose your pokemon!!!")
+                            Image("Eevee")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 300, height: 300)
+                                .clipShape(Circle())
+                                .contextMenu {
+                                    Button("Tackle") {
+                                        
+                                    }
+                                    Button("Swift") {
+                                        
+                                    }
+                                    Button("Double-edge") {
+                                        
+                                    }
+                                }
+                                Image("Pikachu")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 200, height: 200)
+                                    .clipShape(Rectangle())
+                                    .contextMenu {
+                                        Button("Thunderbolt") {
+                                            
+                                        }
+                                        Button("Iron Tail") {
+                                            
+                                        }
+                                        Button("Quick Attack") {
+                                            
+                                        }
+                                        Button("Electro Ball") {
+                                            
+                                        }
+                                    }
+                            
+                            
 
                         }
-                        
-                        
                     }
                     .padding()
                     .navigationTitle("Pokemon Game")
+                }
+                Tab("Battles", systemImage: "bolt.fill") {
+                    List {
+                        Text("Who do you want to battle?")
+                        Button() {
+                            
+                        } label: {
+                            Image("mewtwo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 200, height: 200)
+                                .clipShape(Rectangle())
+                                .contextMenu {
+                                    Text("Mewtwo")
+                                }
+                        }
+                        Button() {
+                            
+                        } label: {
+                            Image("moltres")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 200, height: 200)
+                                .clipShape(Rectangle())
+                                .contextMenu {
+                                    Text("Moltres")
+                                }
+                        }
+                        Button() {
+                            
+                        } label: {
+                            Image("zapdos")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 200, height: 200)
+                                .clipShape(Rectangle())
+                                .contextMenu {
+                                    Text("Zapdos")
+                                }
+                        }
+                        Button() {
+                            
+                        } label: {
+                            Image("articuno")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 200, height: 200)
+                                .clipShape(Rectangle())
+                                .contextMenu {
+                                    Text("Articuno")
+                                }
+                        }
+                    }
+                    
+                    
                 }
             }
             
