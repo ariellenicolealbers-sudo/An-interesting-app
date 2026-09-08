9a5c9b5e24acea367ebe8e594ed08659ea59df69
ariellenicolealbers-sudo <arielle_nicole_albers@s2026.ssts.edu.sg>
2026-09-08 20:23:23 +0800

    its better now right

--- An interesting app/ContentView.swift
+++ An interesting app/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "star")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            TabView {
+                Tab("Home") {
+                    VStack {
+                        Image("Eevee")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300, height: 300)
+                            .clipShape(Circle())
+                        List {
+                            Button("Tackle") {
+                                
+                            }
+                            Button("Swift") {
+                                
+                            }
+                            Button("Double-edge") {
+                                
+                            }
+
+                        }
+                        
+                        
+                    }
+                    .padding()
+                    .navigationTitle("Pokemon Game")
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
