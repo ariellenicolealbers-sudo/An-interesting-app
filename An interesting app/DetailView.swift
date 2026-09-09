91ac54a9c19a89bb0d46fb585e5bac6665c103c1
ariellenicolealbers-sudo <arielle_nicole_albers@s2026.ssts.edu.sg>
2026-09-09 15:37:00 +0800

    Added DetailView and NavigationLink

---
+++ An interesting app/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  An interesting app
+//
+//  Created by Arielle Albers on 9/9/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
